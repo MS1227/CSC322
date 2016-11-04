dda611bab8b62c90c692ea29974d1213ea355ab9
Matt Schnider <matt.schnider@gmail.com>
2016-11-04 12:58:54 -0400

    first commit

--- MyASMHw/Class 11-2-16/p4play.asm
+++ MyASMHw/Class 11-2-16/p4play.asm
@@ -10,6 +10,8 @@ msg                 BYTE "Please enter something: ",0
 inputBuff           BYTE 15 DUP(0)  ;;;; where we will read in a name.
 strSize             DWORD     ?     ;;;; String length.
 
+myData                  BYTE 5 DUP(15 DUP(' '), 0);
+
 .code
 main PROC
      call clrscr
@@ -24,8 +26,27 @@ main PROC
      call ReadString
      mov strSize,eax
 
+     ;;;; CALL OUR STR COPY PROC
+     ;;;;; Params eax = length, esi points to src string, edi point to where we want to copy the string.
+     mov esi, OFFSET inputBuff
+     mov edi, OFFSET myData
+     call strCopy
+
      exit
 
 main ENDP
 
+strCopy PROC
+        pushAD
+        mov ecx, eax ;;; copy str length into loop reg
+TOP:
+        mov bh,[esi]
+        mov [edi],bh
+        inc esi
+        inc edi
+
+        loop TOP
+        popAD
+        ret
+strCopy ENDP
 END main
