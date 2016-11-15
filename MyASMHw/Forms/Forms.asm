78e8264c8ef03af74f1c0746b2b72c962897b364
Matt Schnider <matt.schnider@gmail.com>
2016-11-15 17:34:48 -0500

    commenting complete, final testing and adding printing of 2 digits needed.

--- MyASMHw/Forms/Forms.asm
+++ MyASMHw/Forms/Forms.asm
@@ -8,66 +8,64 @@
 INCLUDE IRVINE32.INC
 
 .data
-textColor           DWORD     green + (lightGray * 16)
-blankColor          DWORD     lightGray + (green * 16)
+textColor           DWORD     green + (lightGray * 16)						;;Text color - green on light gray background
+blankColor          DWORD     lightGray + (green * 16)						;;Inversion of the above to make blank spaces visable.
+												;;//Following strings are stored in memory for use in the program:
+headerStr           BYTE      "Forms Program - By: Matt Schnider",0				;;Header string that will be printed at the top of the window.
+firstStr            BYTE      "First Name:",0							;;First name title string
+lastStr             BYTE      "Last Name:", 0							;;Last name title string
+phoneStr            BYTE      "Phone:", 0							;;Phone number title string
+emailStr            BYTE      "Email:",0							;;Email address title string
+recordStr           BYTE      "Record Count",0							;;Record count string
+pressEnt	          BYTE      "<Enter> by itself to quit.",0				;;String to alert user to press enter to quit
+Space               BYTE      " ",0								;;Blank space.
+firstP		     BYTE      "First Name",0							;;First name string used to title print out column
+lastP		     BYTE      "Last Name",0							;;Last name string used to title print out column
+phoneP		     BYTE      "Phone Number",0							;;Phone number string used to title print out column
+emailP		     BYTE      "Email Address",0						;;Email address string used to title print out column
 
-headerStr           BYTE      "Welcome To A Forms Application - By: Matt Schnider",0
-firstStr            BYTE      "First Name:",0
-lastStr             BYTE      "Last Name:", 0
-phoneStr            BYTE      "Phone:", 0
-emailStr            BYTE      "Email:",0
-recordStr           BYTE      "Record Count",0
-pressEnt	          BYTE      "<Enter> by itself to quit.",0
-Space               BYTE      " ",0
+PrintCount          BYTE      01								;;Integer value initialized to 1 for use in marking line numbers when printing out records
+counter             BYTE      00								;;Integer value initialized to 0 for use in counting number of inserted records
+fifteenSpaces       BYTE      15 DUP(' '), 0							;;15 blank spaces - used to print blanks
+fourteenSpaces      BYTE      14 DUP(' '), 0							;;14 blank spaces
+twentyFiveSpaces    BYTE      25 DUP(' '), 0							;;25 blank spaces
+recordCounter	     BYTE      "Record Count:",0						;;Record count string
+printLine           BYTE      5									;;Integer used to hold the y coordinate of the line being printed on
 
-firstP		     BYTE      "First Name",0
-lastP		     BYTE      "Last Name",0
-phoneP		     BYTE      "Phone Number",0
-emailP		     BYTE      "Email Address",0
-
-PrintCount          BYTE      01
-counter             BYTE      00
-fifteenSpaces       BYTE      15 DUP(' '), 0
-fourteenSpaces      BYTE      14 DUP(' '), 0
-twentyFiveSpaces    BYTE      25 DUP(' '), 0
-recordCounter	     BYTE      "Record Count:",0
-printLine           BYTE      5
-
-theRecords	    BYTE      10 DUP(2 DUP(15 DUP(' '),0), 14 DUP(' '),0, 25 DUP(' '), 0)
-firstBuf	         BYTE      15 DUP(' ')
-lastBuf		    BYTE      15 DUP(' ')
-phoneBuf	         BYTE      14 DUP(' ')
-emailBuf	         BYTE      25 DUP(' ') 
-byteCount	         BYTE      ?
+theRecords	    BYTE      10 DUP(2 DUP(15 DUP(' '),0), 14 DUP(' '),0, 25 DUP(' '), 0)	;;Formatted space in RAM to hold the data for 10 records
+firstBuf	         BYTE      15 DUP(' ')							;;Buffer of size 15 to limit the amount of chars read in from the user input
+lastBuf		    BYTE      15 DUP(' ')							;;Same as above
+phoneBuf	         BYTE      14 DUP(' ')							;;Same as above	- size different
+emailBuf	         BYTE      25 DUP(' ') 							;;Same as above
 
 
 .code
 main PROC
-     mov eax, textColor
-     call SetTextColor
-     call clrscr
+     mov eax, textColor										;;Moves the value of textColor into the eax register
+     call SetTextColor										;;Calls the proc to set the window colors to the values spec'd in the eax
+     call clrscr										;;Clear Screen wipes the screen and shows new colors
 
     
-     mov ecx, 10
+     mov ecx, 10										;;10 is moved into ecx for use in creating a loop to collect the 10 records
      
-     mov edi, OFFSET theRecords
+     mov edi, OFFSET theRecords									;;edi set to point to the beginnin of the records array
 
-RDATA:
-     push ecx
+RDATA:												;;Label for looping purposes
+     push ecx											;;ecx val is pushed onto the stack
 
-     call drawFields
-	mov dl, (80 - SIZEOF fifteenSpaces)/2 +5
-	mov dh, 4
-	call gotoxy
+     call drawFields										;;drawFields is called to print the form to the window
+	mov dl, (80 - SIZEOF fifteenSpaces)/2 +5						;;X coordinate of first name blank
+	mov dh, 4										;;Y coordinate of first name blank
+	call gotoxy										;;Moves cursor to above x,y coords
 
-	mov edx, edi
-	mov ecx, LENGTHOF firstBuf
-	call ReadString
-	add edi, SIZEOF firstBuf+1
-	cmp eax,0
-	je FinishedDataEntry
+	mov edx, edi										;;moves the val of edi into edx
+	mov ecx, LENGTHOF firstBuf								;;moves the length of the firstBuf into the ecx to limit chars read
+	call ReadString										;;ReadString called to prompt user for data entry
+	add edi, SIZEOF firstBuf+1								;;Advances the edi to point to the next field of the array in memory
+	cmp eax,0										;;Compare statement to determine if the user input was null
+	je FinishedDataEntry									;;If above statement was true, jump to FinishedDataEntry, else keep going
 	
-	mov dl,(80 - SIZEOF fifteenSpaces + 1)/2 + 5
+	mov dl,(80 - SIZEOF fifteenSpaces + 1)/2 + 5						;;Same as above
 	mov dh, 6
 	call gotoxy
 	mov edx, edi
@@ -91,62 +89,61 @@ RDATA:
 	call ReadString
 	add edi, SIZEOF emailBuf+1
 
-	inc counter
+	inc counter										;;Record counter is incremented
 
 	
  
-	pop ecx
+	pop ecx											;;ecx is popped off the stack so the following loop command can operate
 loop RDATA
-	call clrscr
 
-FinishedDataEntry:
-     mov eax, textColor
-     call setTextColor
-     call clrscr
-     call drawPrint
-	movzx ecx,counter
-	mov esi, OFFSET theRecords
+FinishedDataEntry:										;;label destination for the above jump instruction
+     mov eax, textColor										;;moves the text color into the eax
+     call setTextColor										;;proc call to set the text color to the one in the eax
+     call clrscr										;;screen is cleared in prep for printing out records
+     call drawPrint										;;drawPrint proc is called to set up the column headers, etc. for printing
+	movzx ecx,counter									;;moves the amount of records stored in counter, padded with leading zeros into the ecx
+	mov esi, OFFSET theRecords								;;to set up a loop for printing. The esi is set to the address of theRecords array.
 
-PrintData:
-     movzx eax, printCount
-     call writeDec
-     movzx eax, space
+PrintData:											;;Label used to print the records out with a loop
+     movzx eax, printCount									;;moves the printCount value into the eax, padded with leading zeros
+     call writeDec										;;WriteDec prints out the decimal value of the number in the eax
+     movzx eax, space										;;this line and the following 2 linesprint 2 spaces
+     call writeChar			
      call writeChar
-     call writeChar
-     call printFirst
+     call printFirst										;;Proc call to print first name
 
-     mov dl, 3 + SIZEOF firstBuf
+     mov dl, 3 + SIZEOF firstBuf								;;x coordinate of first name is moved into the dl 
+     mov dh, printLine										;;y coordinate is set to the current printLine value
+     call gotoxy										;;moves cursor to above x,y coordinate
+     call printLast										;;Proc call to print last name
+
+     mov dl, 3 + SIZEOF firstBuf + SIZEOF lastBuf						;;Same as above
      mov dh, printLine
      call gotoxy
-     call printLast
-
-     mov dl, 3 + SIZEOF firstBuf + SIZEOF lastBuf
-     mov dh, printLine
-     call gotoxy
-     call printPhone
+     call printPhone							
 	
-     mov dl, 3 + SIZEOF firstBuf + SIZEOF lastBuf + SIZEOF phoneBuf
+     mov dl, 3 + SIZEOF firstBuf + SIZEOF lastBuf + SIZEOF phoneBuf		
      mov dh, printLine
      call gotoxy
      call printEmail
 	
-     inc printCount
-     call crlf
+     inc printCount										;;printCount is incremented for the printing of the next record if applicable
+     call crlf											;;Cursor is moved to a new line
 
-     inc printLine
-loop PrintData
-exit
+     inc printLine										;;printline is incremented to properly print the next record
+loop PrintData											;;Loops back to printData if ecx is not equal to 0
+exit									
 
-main ENDP
-printEmail PROC
-     mov edx,esi
-	call WriteString	          ;;email print
-     mov edx, OFFSET Space
+main ENDP																			
+printEmail PROC											;;Procedure for printing out a stored email address referenced by the esi
+     mov edx,esi										;;the address in the esi is moved to the edx
+	call WriteString	          ;;email print						;;WriteString prints out the string located in the edx
+     mov edx, OFFSET Space									;;Space printed
      call WriteString
-	add esi, SIZEOF emailBuf+1
-RET
+	add esi, SIZEOF emailBuf+1								;;esi is updated to point to the next item in theRecords array
+RET												;;Return to calling proc
 PrintEmail ENDP
-printPhone PROC
+printPhone PROC											;;Same as above proc
      mov edx, esi
      call WriteString	          ;;phone number print
      mov edx, OFFSET Space
@@ -173,15 +170,15 @@ printLast PROC
 RET
 printLast ENDP
 
-drawPrint PROC
-     mov dl, (80 - SIZEOF headerStr)/2
-     mov dh, 1
+drawPrint PROC											;;Proc to print out the formatted print out of the data in the array
+     mov dl, (80 - SIZEOF headerStr)/2								;;x and y coords are set up and the cursor is moved to the coordinate.
+     mov dh, 1										
      call gotoxy
 
-     mov edx, OFFSET headerStr
-     call WriteString
+     mov edx, OFFSET headerStr									;;address of headerStr is moved into the edx and printed out by WriteString
+     call WriteString										;;at the above coords
 
-     mov dl, 3
+     mov dl, 3											;;Above process is repeated below print the column headers 
      mov dh,4
      call gotoxy
      
@@ -212,38 +209,38 @@ drawPrint PROC
   RET
 drawPrint ENDP
 
-drawFields PROC
-
-     mov eax, textColor
+drawFields PROC											;;Proc to set up the window with text boxes and box titles for the collection of data
+												;;from the user.
+     mov eax, textColor										;;textColor is set in prep for print out the title
      call setTextColor
 
-     mov dl, (80 - SIZEOF headerStr)/2
+     mov dl, (80 - SIZEOF headerStr)/2								;;cursor is moved to the xy coords of appropriate for the header string
      mov dh, 1
      call gotoxy
-
-     mov edx, OFFSET headerStr
+	
+     mov edx, OFFSET headerStr									;;Header is printed out
      call WriteString
-     mov dl, (80 - SIZEOF firstStr - SIZEOF fifteenSpaces)/2
+     mov dl, (80 - SIZEOF firstStr - SIZEOF fifteenSpaces)/2					;;x-y coords for the first name title
      mov dh,4
      call gotoxy
 
      mov edx, OFFSET firstStr
-     call WriteString						;;Print first name string.
+     call WriteString						;				;Print first name title.
 
-     mov eax, blankColor
+     mov eax, blankColor									;Blank color is set for the creation of pseudo text box.
      call SetTextColor
 
      mov edx, OFFSET fifteenSpaces
-     call WriteString
+     call WriteString										;Pseudo text box printed out.
 
-     mov dl, (80 - SIZEOF lastStr - SIZEOF fifteenSpaces+1)/2
+     mov dl, (80 - SIZEOF lastStr - SIZEOF fifteenSpaces+1)/2					;Same as above, but with different title
      mov dh, 6
      call gotoxy
 
      mov eax, textColor
      call setTextColor
 
-     mov edx, OFFSET lastStr					;;Print last name string.
+     mov edx, OFFSET lastStr					
      call WriteString
 
      mov eax, blankColor
@@ -260,7 +257,7 @@ drawFields PROC
      call setTextColor
 
      mov edx, OFFSET phoneStr
-     call WriteString						;;Print phone string.
+     call WriteString						
 
       mov eax, blankColor
      call setTextColor
@@ -277,7 +274,7 @@ drawFields PROC
      call setTextColor
 
      mov edx, OFFSET emailStr
-     call WriteString						;;Print email string.
+     call WriteString						
 
       mov eax, blankColor
      call setTextColor
@@ -285,30 +282,30 @@ drawFields PROC
      mov edx, OFFSET TwentyFiveSpaces
      call WriteString
      
-     cmp ecx,10
-     jge FIRSTENTRY
+     cmp ecx,10											;;The value in the ecx is compared to 10
+     jge FIRSTENTRY										;;If the value in the ecx is greater than or equal to 10, jump to FIRSTENTRY label
      
-     mov dl, ((80 - SIZEOF emailStr - SIZEOF TwentyFiveSpaces)/2) + 8
-     mov dh, 13
+     mov dl, ((80 - SIZEOF emailStr - SIZEOF TwentyFiveSpaces)/2) + 8				;;This code is reached after one record is read in, it simply prompts the user
+     mov dh, 13											;;to press enter to quit.
      call gotoxy
      mov edx, OFFSET pressEnt
      call WriteString
 
 FIRSTENTRY:
-     mov dl, (80 - 16)						;; X-coordinate of the record counter.
-     mov dh, 24							;; Y-coordinate of the record counter.
-     call gotoxy						;; mov cursor to above coordinates.
+     mov dl, (80 - 16)										;; X-coordinate of the record counter.
+     mov dh, 24											;; Y-coordinate of the record counter.
+     call gotoxy										;; mov cursor to above coordinates.
 
-     mov eax, textColor
+     mov eax, textColor	 									;;textColor is set
      call setTextColor
 
-     mov edx, OFFSET recordCounter
+     mov edx, OFFSET recordCounter								;;record counter is printed out
      call WriteString
 
      mov eax, blankColor
      call setTextColor
 
-     movzx eax, counter
+     movzx eax, counter										;;number of records in the database is printed out
      call WriteDec
 
  
