0d25a3cb381a3dfa8dff7a35c97ba8fdceaa7ca0
Matt Schnider <matt.schnider@gmail.com>
2016-11-30 08:50:37 -0500

    happy finished!

--- MyASMHw/Happy/Happy.asm
+++ MyASMHw/Happy/Happy.asm
@@ -11,19 +11,15 @@ RCC Struct                    ;;;;Row Col Char
      Char      BYTE          'X'
 RCC ENDS
 
-CharAttrib Struct
-	TheChar	  BYTE ' '	; a space
-	TheAttrib   BYTE 7	     ; white on black
-CharAttrib ENDS
-
 .data
 
-
-
 Smiley         RCC  <12,39, 1>
+backC          BYTE      green*16
+foreC          BYTE      blue
 .code
 main PROC
-	mov	 	eax, blue + (green*16)	
+	movsx ax, foreC 
+     add  eax, DWORD PTR backC	
 	call	SetTextColor
 	call 	Clrscr  
      mov esi,0
@@ -52,88 +48,98 @@ L4:
        call moveRight
        jmp L1
 L5:
-     cmp eax, 17h
+     cmp eax, 1071h
 	jne L6    					;q exit
-	je END
+	je LAST
 L6:
-     cmp eax, 15h
+     cmp eax, 1051h
 	jne L7					;Q exit
-	je END
+	je LAST
 L7:
-     cmp eax, 36h
+     cmp eax, 2E63h
 	jne L8
 	call clearHappy
 	jmp L1
-L8:  cmp eax, 34h
+L8:  cmp eax, 2E43h
 	jne L9
 	call clearHappy
 	jmp L1
-L9:  cmp eax, 27h
+L9:  cmp eax, 1372h
 	jne L10
 	call redHappy
 	jmp L1
-L10: cmp eax, 25h
+L10: cmp eax, 1352h
 	jne L11
 	call redBackground
 	jmp L1
-L11: cmp eax, 76h
+L11: cmp eax, 2267h
 	jne L12
 	call greenHappy
 	jmp L1
-L12: cmp eax, 74h
+L12: cmp eax, 2247h
 	jne L13
 	call greenBackground
 	jmp L1
-L13: cmp eax, 26h
+L13: cmp eax, 3062h
 	jne L14
 	call blueHappy
 	jmp L1
-L14: cmp eax, 24h
+L14: cmp eax, 3042h
+     jne LAST
 	call blueBackground
 	jmp L1
 	
-END:
+LAST:
 	exit
 main ENDP
 
 blueBackground PROC
-	call GetTextColor
-	add eax, (blue*16)
+	
+	mov backC, blue*16
+     movsx eax, foreC
+     add eax, DWORD PTR backC
 	call SetTextColor
 	
   RET
 blueBackground ENDP
 blueHappy PROC
-	call GetTextColor
-	add eax, blue
+	mov foreC,blue
+     movsx eax, foreC
+     add eax, DWORD PTR backC
 	call SetTextColor
+
 	call setHappy
  RET
 blueHappy ENDP
 greenBackground PROC
-	call GetTextColor
-	add eax, (green*16)
+	mov backC, green*16
+     movsx eax, foreC
+     add eax, DWORD PTR backC
 	call SetTextColor
+	
 	
   RET
 greenBackground ENDP
 greenHappy PROC
-	call GetTextColor
-	add eax, green
+mov foreC, green
+     movsx eax, foreC
+     add eax, DWORD PTR backC
 	call SetTextColor
 	call setHappy
  RET
 greenHappy ENDP
 redBackground PROC
-	call GetTextColor
-	add eax, (red*16)
+	mov backC, red*16
+     movsx eax, foreC
+     add eax, DWORD PTR backC
 	call SetTextColor
 	
   RET
 redBackground ENDP
 redHappy PROC
-	call GetTextColor
-	add eax, red
+	mov foreC,red
+     movsx eax, foreC
+     add eax, DWORD PTR backC
 	call SetTextColor
 	call setHappy
   RET
@@ -160,8 +166,12 @@ moveRight PROC
      cmp Smiley[esi].col,79
      je noMore
      mov al, ' '
+     mov dl, Smiley[esi].col
+     mov dh, Smiley[esi].row
+     call gotoxy
      call WriteChar
-     inc Smiley[esi].col
+     inc Smiley.col
+     mov dl, Smiley[esi].col
      call gotoxy
      mov al, Smiley[esi].char
      call writeChar
@@ -172,9 +182,14 @@ moveLeft PROC
    
      cmp Smiley[esi].col,0
      je noMore
-     mov al, ' '
+     call gotoxy
+      mov al, ' '
+     mov dl, Smiley[esi].col
+     mov dh, Smiley[esi].row
+     call gotoxy
      call WriteChar
-     dec Smiley[esi].col
+     dec Smiley.col
+     mov dl, Smiley[esi].col
      call gotoxy
      mov al, Smiley[esi].char
      call writeChar
@@ -185,9 +200,13 @@ moveForward PROC
      
      cmp Smiley[esi].row,0
      je noMore
-     mov al, ' ' 
+     mov al, ' '
+     mov dl, Smiley[esi].col
+     mov dh, Smiley[esi].row
+     call gotoxy
      call WriteChar
-     dec Smiley[esi].row
+     dec Smiley.row
+     mov dh, Smiley[esi].row
      call gotoxy
      mov al, Smiley[esi].char
      call writeChar
@@ -196,14 +215,19 @@ RET
 moveForward ENDP
 moveBack PROC
      
-     cmp Smiley[esi].row,79
+     cmp Smiley[esi].row,24
      je noMore
      mov al, ' '
+     mov dl, Smiley[esi].col
+     mov dh, Smiley[esi].row
+     call gotoxy
      call WriteChar
-     inc Smiley[esi].row
+     inc Smiley.row
+     mov dh, Smiley[esi].row
      call gotoxy
      mov al, Smiley[esi].char
      call writeChar
+noMore:
 RET
 moveBack ENDP
 END main
